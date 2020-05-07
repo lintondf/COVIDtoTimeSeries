8aa5ae36a782e72a2ce33fedc8c6517ff039160e
Donald Linton <lintond@gmail.com>
2020-05-07 17:20:00 -0400

    cleanup

--- covid/Counties.py
+++ covid/Counties.py
@@ -30,8 +30,8 @@ from sortedcontainers import SortedSet
 from Population import loadPopulation, loadStatePopulations
 from Deaths import updateDeaths
 from IHME import IHME
-from astropy.wcs.docstrings import row
 
+import plotly.figure_factory as ff
 
 def main(dataPath : str):
     path = dataPath + 'US-Counties-Population.xlsx'
@@ -42,4 +42,29 @@ def main(dataPath : str):
     
 if __name__ == '__main__':
     home = os.path.expanduser('~')
-    main(home + '/GITHUB/COVIDtoTimeSeries/data/')
+#     main(home + '/GITHUB/COVIDtoTimeSeries/data/')
+    df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/laucnty16.csv')
+    df_sample['State FIPS Code'] = df_sample['State FIPS Code'].apply(lambda x: str(x).zfill(2))
+    df_sample['County FIPS Code'] = df_sample['County FIPS Code'].apply(lambda x: str(x).zfill(3))
+    df_sample['FIPS'] = df_sample['State FIPS Code'] + df_sample['County FIPS Code']
+    
+    colorscale = ["#f7fbff", "#ebf3fb", "#deebf7", "#d2e3f3", "#c6dbef", "#b3d2e9", "#9ecae1",
+        "#85bcdb", "#6baed6", "#57a0ce", "#4292c6", "#3082be", "#2171b5", "#1361a9",
+        "#08519c", "#0b4083", "#08306b"
+    ]
+    endpts = list(np.linspace(1, 12, len(colorscale) - 1))
+    fips = df_sample['FIPS'].tolist()
+    values = df_sample['Unemployment Rate (%)'].tolist()
+    
+    
+    fig = ff.create_choropleth(
+        fips=fips, values=values, scope=['usa'],
+        binning_endpoints=endpts, colorscale=colorscale,
+        show_state_data=False,
+        show_hover=True,
+        asp = 2.9,
+        title_text = 'USA by Unemployment %',
+        legend_title = '% unemployed'
+    )
+    fig.layout.template = None
+    fig.show()
